8e0494623b2f56771f74869b7f03a99df718d27b
FabricioGodoy <fabriciogodoy95@hotmail.com>
2021-11-01 23:27:02 -0300

    prueba

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-// const publicPath = path.resolve(__dirname, './public')
-// app.use(express.static(publicPath)) ;
+ const publicPath = path.resolve(__dirname, './public')
+ app.use(express.static(publicPath));
 
 app.get('/', (req, res) =>{
     res.sendFile(path.resolve(__dirname, './views/home.html'))
